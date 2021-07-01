07ef9339caf647da814da8f5761e8beb826d26df
mongoy <mongoy@rambler.ru>
2021-07-01 11:49:36 +0900

    add models in models.py and registred them

--- app_api/admin.py
+++ app_api/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    """ Опросы """
+    list_display = [field.name for field in Survey._meta.fields]  # все поля выводит в цикле
+    search_fields = ["title"]
+    list_filter = ["title", "data_start"]
+
+
+@admin.register(TypeQuestion)
+class TypeQuestionAdmin(admin.ModelAdmin):
+    """ Типы вопросов """
+    list_display = ("id", "name",)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """ Типы вопросов """
+    list_display = [field.name for field in Question._meta.fields]  # все поля выводит в цикле
+    search_fields = ["text_question"]
+    list_filter = ["text_question", "survey", "type_question"]
+
+
+@admin.register(Interviewee)
+class IntervieweeAdmin(admin.ModelAdmin):
+    """ Пользовавтель """
+    list_display = ("id",)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    """ Ответы """
+    list_display = [field.name for field in Question._meta.fields]  # все поля выводит в цикле
+    search_fields = ["user_id", "survey", "text"]
+    list_filter = ["user_id", "survey", "text"]
--- app_api/models.py
+++ app_api/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Survey(models.Model):
+    """ Опрос """
+    title = models.CharField(max_length=250, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', blank=True)
+    data_start = models.DateField(verbose_name='Дата начала', editable=False)
+    data_stop = models.DateField(verbose_name='Дата окончания')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        """Вид в админке"""
+        verbose_name = "Опрос"
+        verbose_name_plural = "Опросы"
+        ordering = ['-data_start']
+
+
+class TypeQuestion(models.Model):
+    """ Тип вопроса """
+    name = models.CharField(max_length=100, unique=True, db_index=True, verbose_name='Тип вопроса')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        """Вид в админке"""
+        verbose_name = "Тип вопроса"
+        verbose_name_plural = "Типы вопросов"
+        ordering = ['name']
+
+
+class Question(models.Model):
+    """ Вопрос """
+    survey = models.ForeignKey(Survey, on_delete=models.PROTECT, verbose_name='Опрос')
+    type_question = models.ForeignKey(TypeQuestion, on_delete=models.PROTECT, verbose_name='Тип вопроса')
+    text_question = models.TextField(verbose_name='Текст вопроса')
+    text_answer = models.TextField(verbose_name='Текст ответа')
+
+    def __str__(self):
+        return self.text_question
+
+    class Meta:
+        """Вид в админке"""
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
+
+
+class Interviewee(models.Model):
+    """ Пользовавтель """
+    id = models.IntegerField(max_length=9)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        """ Вид в админке """
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+class Answer(models.Model):
+    """ Ответы """
+    user_id = models.ForeignKey(Interviewee, on_delete=models.PROTECT, verbose_name='Пользователь')
+    survey = models.ForeignKey(Survey, on_delete=models.PROTECT, verbose_name='Опрос')
+    text = models.CharField(verbose_name='Содержимое ответа')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        """ Вид в админке """
+        verbose_name = "Ответ"
+        verbose_name_plural = "Ответы"
